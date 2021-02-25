1f9577f2c7d5b59a9ceab8787d41efd18650cb87
Trouni Tiet <trouni@gmail.com>
2021-02-26 02:26:30 +0900

    Complete prep with collapsible form in Stimulus and Ajax

---
+++ app/javascript/controllers/collapsible_form_controller.js
@@ -0,0 +1,23 @@
+import { Controller } from "stimulus";
+
+export default class extends Controller {
+  static targets = [ 'form' ];
+
+  connect() {
+    this.formTarget.style.height = "0"
+    this.formTarget.style.overflow = "hidden"
+    this.formTarget.style.transition = "height 0.2s ease-in"
+  }
+
+  expandForm(event) {
+    this.formTarget.style.height = "150px"
+    event.currentTarget.hidden = true
+  }
+
+  submitOnEnter(event) {
+    if (event.key === 'Enter' && !event.shiftKey) {
+      event.preventDefault()
+      this.formTarget.submit()
+    }
+  }
+}
